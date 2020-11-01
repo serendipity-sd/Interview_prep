406c7cd6916cc6b0c19ed8e1e51202db684813a6
Suprit_Deepak <deepak.suprit@gmail.com>
2020-11-01 14:10:49 +0530

    Create perplexity.py

---
+++ NLP/perplexity.py
@@ -0,0 +1,13 @@
+"""
+The cross-entropy of a unigram model is 9.91. What is its perplexity?
+
+Answer correctly to the nearest integer. You may either submit the final answer directly in the plain-text mode, or you may write a program which computes and displays the answer in the language of your choice.
+
+Your answer should look like this:
+
+881
+Do not use any leading or trailing spaces, tabs or newlines.
+"""
+
+entropy =  9.91
+print(2** entropy)
