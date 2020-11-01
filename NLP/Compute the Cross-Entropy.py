d5efac6720270613f652c51f68c1eb5eca820aa4
Suprit_Deepak <deepak.suprit@gmail.com>
2020-11-01 14:15:55 +0530

    Create Compute the Cross-Entropy.py

---
+++ NLP/Compute the Cross-Entropy.py
@@ -0,0 +1,14 @@
+"""
+The perplexity of a bigram model is 170. Compute its cross-entropy corrected to 2 decimal places.
+
+You may either submit the final answer in the plain-text mode, or you may submit a program in the language of your choice to compute the required value.
+
+Your answer should look like this:
+
+5.50  
+Do not use any extra leading or trailing spaces or newlines.
+"""
+
+
+import math
+print(math.log2(170))
